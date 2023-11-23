06f003ec4f50b3b22ab5ed327d9369b4f07858b1
jasmeet singh <jskainthofficial@gmail.com>
2023-11-23 16:54:58 +0530

    :rocket :+ pwa support

---
+++ wwwroot/service-worker.js
@@ -0,0 +1,34 @@
+﻿const CACHE_NAME = 'almond-cache-v2';
+const cacheUrls = [
+    // Add more URLs to cache here
+    '/assets/css/theme.min.css',
+    '/assets/js/theme.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(cacheUrls);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
